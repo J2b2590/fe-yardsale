de60a159929c4e568c32972ab18162f89b0b9c63
J2b2590 <J2b2590@gmail.com>
2021-02-22 11:28:47 -0600

    working on login handle submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,10 +3,33 @@ import React, { Component } from "react";
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      username: "",
+    };
   }
+
+  handleChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
-    return <div>THIS IS LOGIN</div>;
+    return (
+      <div>
+        THIS IS LOGIN
+        <form>
+          <input
+            type="text"
+            name="username"
+            placeholder="username sign in"
+            value={this.props.username}
+            onChange={(e) => this.handleChange(e)}
+          />
+        </form>
+      </div>
+    );
   }
 }
 
