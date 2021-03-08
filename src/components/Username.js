51b9d3f99ecad22dc2cd46f28df8679b277cbb35
J2b2590 <J2b2590@gmail.com>
2021-03-08 09:23:16 -0600

    made username a functional component instead of class

--- src/components/Username.js
+++ src/components/Username.js
@@ -1,20 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-class Username extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    console.log(this.props.user, "USERNAME");
-    const currentUsername = this.props.user.name;
-    return (
-      <div>
-        <h2>{currentUsername}</h2>
-      </div>
-    );
-  }
+function Username(props) {
+  console.log(props, "USERNAME");
+  const currentUsername = props.user.name;
+  return (
+    <div>
+      <h2>{currentUsername}</h2>
+    </div>
+  );
 }
 
 const mapStateProps = (state) => {
